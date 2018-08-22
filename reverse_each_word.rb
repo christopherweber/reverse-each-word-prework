07c55be1ffac3493a643d10ef667c3166bfb8ef2
Christopher Weber <chrisrrweber@gmail.com>
2018-08-22 16:33:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,12 +7,5 @@ def reverse_each_word(array)
    arr2.join(" ")
 end
 
-def reverse_each_word
-    arr2 = []
-  arr = array.split
-  arr.collect do | x |
-   arr2  << x.reverse
- end
-   arr2.join(" ")
-end
+
 
